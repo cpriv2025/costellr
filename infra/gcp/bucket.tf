5682d4384c86c4089b284608c06ae4f0bb4d123e
abb072024 <mail@example.com>
2025-01-31 09:50:54 +0100

    New

--- infra/gcp/bucket.tf
+++ infra/gcp/bucket.tf
@@ -2,5 +2,5 @@ resource "google_storage_bucket" "data" {
   name          = "costellr-data-perf"
   location      = "us-west1"
   storage_class = "STANDARD"
-  #force_destroy = true
+  force_destroy = true
 }
